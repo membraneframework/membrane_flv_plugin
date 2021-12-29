0d24a0755c722dc04664720b83014911695755dd
Daniel Jodłoś <daniel.jodlos@swmansion.com>
2021-12-29 07:05:55 +0100

    Updating examples

--- examples/demuxer.exs
+++ examples/demuxer.exs
@@ -3,18 +3,20 @@ Mix.install([
   :membrane_core,
   {:membrane_flv_plugin, path: __DIR__ |> Path.join("..") |> Path.expand()},
   :membrane_file_plugin,
-  {:membrane_aac_plugin, github: "membraneframework/membrane_aac_plugin", branch: "support-remotestream-caps"},
-  {:membrane_h264_ffmpeg_plugin, github: "membraneframework/membrane_h264_ffmpeg_plugin", branch: "support-remote-caps"}
+  :membrane_aac_plugin,
+  :membrane_h264_ffmpeg_plugin
 ])
 
 defmodule Example do
   use Membrane.Pipeline
 
+  @input_file __DIR__ |> Path.join("../test/fixtures/reference.flv") |> Path.expand()
+
   @impl true
-  def handle_init(input_file) do
+  def handle_init(_opts) do
     spec = %ParentSpec{
       children: [
-        src: %Membrane.File.Source{location: input_file},
+        src: %Membrane.File.Source{location: @input_file},
         demuxer: Membrane.FLV.Demuxer,
         video_sink: %Membrane.File.Sink{location: "video.h264"},
         audio_sink: %Membrane.File.Sink{location: "audio.aac"},
@@ -28,24 +30,35 @@ defmodule Example do
       ],
       links: [
         link(:src) |> to(:demuxer),
-
-        # Mind you can prelink the pads if you know the stream id that you are interested in
         link(:demuxer) |> via_out(Pad.ref(:audio, 0)) |> to(:audio_parser) |> to(:audio_sink),
         link(:demuxer) |> via_out(Pad.ref(:video, 0)) |> to(:video_parser) |> to(:video_sink)
       ]
     }
 
-    {{:ok, spec: spec}, %{}}
+    {{:ok, spec: spec}, %{eos_counter: 0}}
   end
+
+  # the rest of the Example module is only used for termination of the pipeline after processing finishes
+  @impl true
+  def handle_element_end_of_stream({sink, _pad}, _ctx, state) when sink in [:audio_sink, :video_sink] do
+    if state.eos_counter == 1 do
+      __MODULE__.stop_and_terminate(self())
+    end
+    {:ok, Map.update!(state, :eos_counter, & &1 + 1)}
+  end
+
+  @impl true
+  def handle_element_end_of_stream(_element, _ctx, state), do: {:ok, state}
 end
 
-ref =
-  Example.start_link("test/fixtures/input.flv")
-  |> elem(1)
-  |> tap(&Membrane.Pipeline.play/1)
-  |> then(&Process.monitor/1)
+# Initialize the pipeline and start it
+{:ok, pid} = Example.start_link()
+:ok = Membrane.Pipeline.play(pid)
 
+monitor_ref = Process.monitor(pid)
+
+# Wait for the pipeline to finish
 receive do
-  {:DOWN, ^ref, :process, _pid, _reason} ->
+  {:DOWN, ^monitor_ref, :process, _pid, _reason} ->
     :ok
 end

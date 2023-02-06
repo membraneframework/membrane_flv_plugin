b2b2cc0d22294aadd9fe90caee744602fd0f5c26
Dominik Artur Wołek <dominik.wolek@swmansion.com>
2023-02-06 11:51:38 +0100

    Bump version to 0.4.1

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.FLV.Mixfile do
   use Mix.Project
 
-  @version "0.4.0"
+  @version "0.4.1"
   @github_url "https://github.com/membraneframework/membrane_flv_plugin"
 
   def project do
@@ -40,11 +40,12 @@ defmodule Membrane.FLV.Mixfile do
       {:membrane_core, "~> 0.11.2"},
       {:membrane_aac_format, "~> 0.7.0"},
       {:membrane_mp4_format, "~> 0.7.0"},
-      {:membrane_h264_format, "~> 0.4.0"},
+      {:membrane_h264_format, "~> 0.5.0"},
       {:membrane_file_plugin, "~> 0.13.2", only: :test},
-      {:membrane_h264_ffmpeg_plugin, "~> 0.25.1", only: :test},
+      {:membrane_h264_ffmpeg_plugin, "~> 0.25.3", only: :test},
       {:membrane_aac_plugin, "~> 0.13.0", only: :test},
-      {:membrane_mp4_plugin, "~> 0.18.0", only: :test},
+      {:membrane_mp4_plugin,
+       github: "membraneframework/membrane_mp4_plugin", branch: "bump-dependencies", only: :test},
       {:bimap, "~> 1.2"},
       {:bunch, "~> 1.3"},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},

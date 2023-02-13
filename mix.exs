eda9995d1b826d3d37828a366a5ecdb06107b312
Dominik Artur Wołek <dominik.wolek@swmansion.com>
2023-02-13 10:23:04 +0100

    Bump version to 0.5.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.FLV.Mixfile do
   use Mix.Project
 
-  @version "0.4.1"
+  @version "0.5.0"
   @github_url "https://github.com/membraneframework/membrane_flv_plugin"
 
   def project do
